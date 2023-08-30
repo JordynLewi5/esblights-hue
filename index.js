e073553e9bb5f9dc79d4265658fdcc8b87f44d3f
JordynLewi5 <jlewis162030@gmail.com>
2023-08-29 22:26:52 -0500

    End time edit

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const Light = require('./src/Light.js');
 const lightInfo1 = {
     bridgeIP: '192.168.123.253',
     username: 'kPaxKx0IAHRcoYJFqbEr9SmdJk4tieJAO1OdznEX',
-    id: 5
+    id: 18
 }
 
-// Follow this guide linenew Light(lightInfo, initial_power_state, [start_hour, end_hour], interval_time_in_ms);
-light = new Light(lightInfo1, false, ['sunset', Util.getCurrentTimeInDecimal() + 0.001], 30 * 1000);
+// Hint: new Light(lightInfo, initial_power_state, [start_hour, end_hour], interval_time_in_ms);
+new Light(lightInfo1, false, ['sunset', 3], 30 * 1000);
